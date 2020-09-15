9678fe1e908273b022cee8a937865ef7c52aa84a
karun <karunahuja1998@gmail.com>
2020-09-15 21:05:37 +0530

    Creating REST API using Spring Boot | Creating GET functionality

---
+++ src/main/java/com/api/book/Examplesecurity2Application.java
@@ -0,0 +1,13 @@
+package com.api.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examplesecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examplesecurity2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/api/book/services/BookService.java
@@ -0,0 +1,54 @@
+package com.api.book.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.api.book.entities.Book;
+
+
+@Component
+public class BookService {
+	
+	private static List<Book> list=new ArrayList<>();
+	
+	static {
+		
+		
+		list.add(new Book(12,"Java Complete","XYZ"));
+		
+		list.add(new Book(13,"python","ABC"));
+		
+		
+		list.add(new Book(14,"Java not  Complete","XYsZ"));
+		
+		list.add(new Book(15,"pythons","AaBC"));
+		
+		
+		
+	}
+
+	public static List<Book> getAllBooks() {
+		return list;
+	}
+	
+	
+	public static Book getBookById(int id) {
+		Book book=null;
+		
+		book=list.stream().filter(e->e.getId()==id).findFirst().get();
+		
+		return book;
+	}
+
+
+	public static void setList(List<Book> list) {
+		BookService.list = list;
+	}
+	
+	
+	
+	
+
+}
