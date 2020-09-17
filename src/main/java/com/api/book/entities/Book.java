691563d59503cc9036f21752d3fef287d574132a
karun <karunahuja1998@gmail.com>
2020-09-17 08:30:53 +0530

    Nesting Custom Type in spring Boot Rest Api | Using ONE TO ONE Mapping with Spring Boot

--- src/main/java/com/api/book/entities/Book.java
+++ src/main/java/com/api/book/entities/Book.java
@@ -1,10 +1,12 @@
 package com.api.book.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,45 +14,62 @@ import javax.persistence.Table;
 public class Book {
 
 	@Id
-	@Column(name="book_id")
 	private int id;
+	
 	private String title;
-	private String author;
 	
-	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
+
+
 	public int getId() {
 		return id;
 	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
 	public String getTitle() {
 		return title;
 	}
+
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getAuthor() {
+
+
+	public Author getAuthor() {
 		return author;
 	}
-	public void setAuthor(String author) {
+
+
+	public void setAuthor(Author author) {
 		this.author = author;
 	}
-	public Book(int id, String title, String author) {
+
+
+	public Book(int id, String title, Author author) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.author = author;
 	}
+
+
 	public Book() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+
 	@Override
 	public String toString() {
 		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
 	}
-	
 	
 	
 }
